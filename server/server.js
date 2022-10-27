d7edc892ab842ef8a45b6b5eb81bba1c457be311
pelshad <akuaru@gmail.com>
2022-10-27 10:50:36 +0900

    sql 연결 및 테스트 완

--- server/server.js
+++ server/server.js
@@ -2,15 +2,35 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-
 const app = express(); // 백엔드 정의
-
 const server = require('http').createServer(app);
+
+// sql 연결
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'coren',
+  database: 'test'
+});
+
+con.connect(function(err){
+  if (err) throw err;
+  console.log('Connected');
+  const sql = "select * from member";
+  con.query(sql, (err, result, fields) => {
+    if (err) throw err;
+    console.log(result)
+  });
+} );
+
+//
 
 app.use(cors()); // cors 미들웨어를 삽입합니다.
 
 app.get('/', (req,res) => { // 요청패스에 대한 콜백함수를 넣어줍니다.
-  res.send({message:'hello'});
+  res.send({message:'helloworld'});
 });
 
 server.listen(8080, ()=>{
